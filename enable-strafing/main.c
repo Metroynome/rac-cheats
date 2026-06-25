ce3186a9b1b14185f2e73cb674d1f7225d637ae2
Metroynome <yort124@gmail.com>
2026-06-25 18:42:55 -0400

    hijack dobehabior

--- enable-strafing/main.c
+++ enable-strafing/main.c
@@ -8,6 +8,9 @@
 #include <librac1/utils.h>
 #include <librac1/player.h>
 
+#define doBehavior_jal ((u32)0x00231dd0)
+#define doBehavior_func ((u32)0x002370b8)
+
 // VariableAddress_t vaUpdateAnalog = {
 // 	.Veldin1 = 0x0020889c,
 // 	.Novalis = 0x00229f9c,
@@ -34,8 +37,9 @@
 typedef void (*CaseHandler)(void);
 static CaseHandler originalHandlers[6] = {0};
 
-void strafeLogic(void)
+void strafe_jr(u32 jumpTo)
 {
+    return;
     // figure out which state we're in to call the right original handler
     Player *player = (Player*)PLAYER_1_STRUCT;
     int state = player->state;
@@ -51,19 +55,41 @@ void strafeLogic(void)
     *playerX = saved;
 }
 
+void strafe_hijack(void)
+{
+    Player *player = (Player*)PLAYER_1_STRUCT;
+    int state = player->state;
+    if (state == 0 || state == 1 || state == 2 || state == 4 || state == 5) {
+        float *playerX = (float*)((u32)player + 0x180);
+        float *camY    = (float*)0x00167258;
+        float f1       = *(float*)0x0015ed64;
+        float momentum = *(float*)0x0015ed6c;
+
+        float saved = *playerX;
+        *playerX = *camY;
+        ((void(*)(int, float, float, float))0x00232490)(0, f1 * 0.017f, f1 * 0.3f, momentum * 4.712389f);
+        *playerX = saved;
+    } else {
+        ((void(*)(void))doBehavior_func)();
+    }
+}
+
 int main(void)
 {
     u32 *jumpTable = (u32*)0x00208e80;
+    u32 jump_jr = 0x00237134;
+
 
     Hero *hero = (Hero*)PLAYER_1_STRUCT;
-    if (
-        hero->state == 0
-        || hero->state == 1
-        || hero->state == 2
-        || hero->state == 4
-        || hero->state == 5
-    ) {
-        strafeLogic();
+    // overwrite jump_jr
+    // if (*(u32*)jump_jr != 0 && *(u32*)jump_jr != ADDR2JAL(&strafe_jr)) {
+    //     HOOK_J(jump_jr, &strafe_jr);
+    // }
+
+    // do behavior hhijack
+    if (*(u32*)doBehavior_jal != 0 && *(u32*)doBehavior_jal == 0x0c08dc2e) {
+        HOOK_JAL(doBehavior_jal, &strafe_hijack);
     }
+
     return 0;
 }
